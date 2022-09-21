c4a52659541fd56254d717745866a4cf099156ba
Gabriel Miguel Dias <112181260+gabrielmigueldias@users.noreply.github.com>
2022-09-21 13:44:05 -0300

    Create script4_01.js

---
+++ aula4/script4_01.js
@@ -0,0 +1,12 @@
+let x, y ,z;
+y=3;
+z=5;
+x = 2 * y + z;
+document.write("x = ",x,"<br>");
+document.write("x = "+x+"<br>");
+document.write (`x = ${x}<br>`);
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Priv
